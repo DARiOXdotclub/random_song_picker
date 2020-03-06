3db357cdaf1e6ec5a242ead353490e5a178f800e
Jyles Coad-Ward <jyles@dariox.club>
2020-03-06 18:07:40 +0800

    Create song_picker.php

---
+++ song_picker.php
@@ -0,0 +1,25 @@
+<?php
+	
+	function marqueeGen($content) {
+		return `
+		<center class="song" >
+		    <marquee class="music fade-in-fwd" width="250px" direction="left" scrollamount="3" behavior="scroll">
+		    	Currently Playing:  `.$content.`
+		    </marquee>
+		</center>`;
+	}
+	function randomSongPicker() {
+		$songNames = json_decode(file_get_contents(""),true);
+		$songURLs = json_decode(file_get_contents(""),true);
+		
+		$randomInt = mt_rand(0,count($songURLs));
+
+		$marquee = $songNames[$randomInt];
+
+		$songChoice = "https://storage.googleapis.com/cdn.jyles.club/pageaudio/".$songURLs[$randomInt];
+
+		$iframe = '<iframe frameborder="0" style="position:absolute;top:5px;left:5px;" src="'.$songChoice.'" allow="autoplay" height="0" width="0" id="iframe"></iframe>';
+		$final = $marquee.$iframe;
+		return $final;
+	}
+?>
