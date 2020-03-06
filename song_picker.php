1f742711b7047c354ad51842bd99961f4cf9a796
Jyles Coad-Ward <jyles@dariox.club>
2020-03-06 18:24:43 +0800

    Update song_picker.php

--- song_picker.php
+++ song_picker.php
@@ -9,7 +9,7 @@
 		</center>`;
 	}
 	function randomSongPicker() {
-		$songNames = json_decode(file_get_contents("https://dxcdn.net/random_song_picker/namelinks.txt"),true);
+		$songNames = file_get_contents("https://dxcdn.net/random_song_picker/namelinks.txt");
 		$songURLs = json_decode(file_get_contents("https://dxcdn.net/random_song_picker/file.json"),true);
 		
 		$randomInt = mt_rand(0,count($songURLs));
